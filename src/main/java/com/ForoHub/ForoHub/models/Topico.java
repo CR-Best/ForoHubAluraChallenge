ec63a702d2a26ff4354677d9b6b3f3e87c094ba6
CR-Best <crosales3103@gmail.com>
2025-01-01 18:06:51 -0600

    Se crea modelo Topico

---
+++ src/main/java/com/ForoHub/ForoHub/models/Topico.java
@@ -0,0 +1,81 @@
+package com.ForoHub.ForoHub.models;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "topicos")
+public class Topico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+
+    private String mensaje;
+
+    private LocalDateTime fechaCreacion = LocalDateTime.now();
+
+    @Enumerated(EnumType.STRING)
+    private EstadoTopico estado = EstadoTopico.ABIERTO;
+
+    @ManyToOne
+    private Usuario autor;
+
+    @ManyToOne
+    private Curso curso;
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public LocalDateTime getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public EstadoTopico getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoTopico estado) {
+        this.estado = estado;
+    }
+
+    public Usuario getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Usuario autor) {
+        this.autor = autor;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+}
